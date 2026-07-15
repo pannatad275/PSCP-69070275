bd2285ca0825e39255b2eb88345101a890ef1d0b
pannatad275 <69070275@kmitl.ac.th>
2026-07-15 22:25:55 +0700

    Create 3024.py

---
+++ 3024/3024.py
@@ -0,0 +1,18 @@
+'''SurprisingVote'''
+def main():
+    '''SurprisingVote'''
+    t_score = float(input())
+    max_score = float(input())
+
+    # t_score = max_score + (min_score / 2)
+    min_score = t_score - (max_score * 2)
+
+    if min_score < 0:
+        min_score = 0
+
+    if max_score - min_score > 2:
+        print("Surprising")
+    else:
+        print("Not surpirsing")
+
+main()
