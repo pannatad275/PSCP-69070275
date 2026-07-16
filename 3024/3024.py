fd38547c8651873d563dc54dfebadcaae7fe201e
pannatad275 <69070275@kmitl.ac.th>
2026-07-16 22:21:31 +0700

    Update 3024.py

--- 3024/3024.py
+++ 3024/3024.py
@@ -7,12 +7,9 @@ def main():
     # t_score = max_score + (min_score / 2)
     min_score = t_score - (max_score * 2)
 
-    if min_score < 0:
-        min_score = 0
-
-    if max_score - min_score > 2:
+    if max_score - min_score >= 2:
         print("Surprising")
     else:
-        print("Not surpirsing")
+        print("Not surprising")
 
 main()
