6e65d96b0e005274f03da25de3dc03a24c3fc152
pannatad275 <69070275@kmitl.ac.th>
2026-07-16 20:52:09 +0700

    Update 3011.py

--- 3011/3011.py
+++ 3011/3011.py
@@ -2,14 +2,16 @@
 c1 = input()
 c2 = input()
 
-if c1 == c2:
-    print(c1)
-elif (c1 == "Red" and c2 == "Yellow") or (c1 == "Yellow" and c2 == "Red"):
-    print("Orange")
-elif (c1 == "Red" and c2 == "Blue") or (c1 == "Blue" and c2 == "Red"):
-    print("Violet")
-elif (c1 == "Blue" and c2 == "Yellow") or (c1 == "Yellow" and c2 == "Blue"):
-    print("Green")
+
+if c1 and c2 in ["Red", "Yellow", "Blue"]:
+    if c1 == c2:
+        print(c1)
+    elif (c1 == "Red" and c2 == "Yellow") or (c1 == "Yellow" and c2 == "Red"):
+        print("Orange")
+    elif (c1 == "Red" and c2 == "Blue") or (c1 == "Blue" and c2 == "Red"):
+        print("Violet")
+    elif (c1 == "Blue" and c2 == "Yellow") or (c1 == "Yellow" and c2 == "Blue"):
+        print("Green")
 else:
     print("Error")
     
