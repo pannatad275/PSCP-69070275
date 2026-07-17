a4ff814e85fe2a462ceb0a345ac55c0e861ce36f
- <ppzz@gmial.com>
2026-07-17 12:53:32 +0700

    Update 3037.py

--- work/3037.py
+++ work/3037.py
@@ -1,13 +1,14 @@
 '''max'''
 def main():
+    '''max'''
     a = int(input())
     b = int(input())
     c = int(input())
 
-    if a > b and a > c:
+    if a >= b and a >= c:
         print(a)
-    elif b > a and b > c:
+    elif b >= a and b >= c:
         print(b)
-    elif c > a and c > b:
+    elif c >= a and c >= b:
         print(c)
 main()
