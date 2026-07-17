394ce374eab453dd46f3bcc260df43b13197ebf6
- <ppzz@gmial.com>
2026-07-17 12:53:30 +0700

    Create 3033.py

---
+++ work/3033.py
@@ -0,0 +1,14 @@
+'''present'''
+import math as m
+def main():
+    '''present'''
+    a, b, c = input().split(" ")
+    d = float(a)
+    e = float(b)
+    f = float(c)
+
+    x = 
+
+
+    print(f"{x:.2f},{y:.2f}")
+main()
