40945d9d8841e9e608b33c59afc6b5b8017ae678
pannatad275 <69070275@kmitl.ac.th>
2026-07-20 20:49:18 +0700

    Update 3032.py

--- work/3032.py
+++ work/3032.py
@@ -1,12 +1,16 @@
 '''score'''
 def main():
     '''score'''
-    bunny = int(input())
-    for _ in range (bunny):
-            score = int(input())
-            
-    max_score = max(score)
-    count_max_score = score.count(max_score)
+    n = int(input())
+    scores = []
+
+    for _ in range(n):
+        score = int(input())
+        scores.append(score)
+
+    max_score = max(scores)
+    count_max = scores.count(max_score)
+
     print(max_score)
-    print(count_max_score)
+    print(count_max)
 main()
