180d41157cb692c031cd3956903f3805ffe1005a
pannatad275 <69070275@kmitl.ac.th>
2026-07-20 23:12:32 +0700

    s

--- work/3030.py
+++ work/3030.py
@@ -1,20 +1,23 @@
 '''saitama'''
+import math as m
 def main():
     '''saitama'''
     a = int(input())
     b = int(input())
     c = int(input())
     d = int(input())
+    e = int(input())
     f = int(input())
     g = int(input())
     h = int(input())
-    i = int(input())
+    if  min(a, b, c, d , e, f , g, h) > 0:
+        daya = m.ceil(a / e)
+        dayb = m.ceil(b / f)
+        dayc = m.ceil(c / h)
+        dayd = m.ceil(d / g)
+        min_day = max(daya, dayb, dayc, dayd)
+    else:
+        min_day = 0
 
-    daya = a / f
-    dayb = b / g
-    dayc = c / h
-    dayd = d / i
-
-    min_day = max(daya, dayb, dayc, dayd)
-    print(f"{min_day:.0f}")
+    print(min_day)
 main()
