cb43b9fd1d0c8ee1b9b91d971202423871aa209e
pannatad275 <69070275@kmitl.ac.th>
2026-07-22 20:04:15 +0700

    หห

--- 3022/3022.py
+++ 3022/3022.py
@@ -1,24 +1,27 @@
-'''Temperature'''
-temp = float(input())
-u = str(input())
-uw = str(input())
+'''temperature'''
+def main():
+    '''Temperature'''
+    temp = float(input())
+    unit = str(input())
+    unit_w = str(input())
 
-if u == "C":
-    temp_unit = temp
-elif u == "F":
-    temp_unit = (temp - 32) * (5 / 9)
-elif u == "K":
-    temp_unit = temp - 273.15
-elif u == "R":
-    temp_unit = (temp * (5 / 9)) -273.15
+    if unit == "C":
+        temp_unit = temp
+    elif unit == "F":
+        temp_unit = (temp - 32) * (5 / 9)
+    elif unit == "K":
+        temp_unit = temp - 273.15
+    elif unit == "R":
+        temp_unit = (temp * (5 / 9)) -273.15
 
-if uw == "C":
-    total = temp_unit
-elif uw == "F":
-    total = (temp_unit * (9 / 5)) + 32
-elif uw == "K":
-    total = temp_unit + 273.15
-elif uw == "R":
-    total = (temp_unit + 273.15) * (9 / 5)
+    if unit_w == "C":
+        total = temp_unit
+    elif unit_w == "F":
+        total = (temp_unit * (9 / 5)) + 32
+    elif unit_w == "K":
+        total = temp_unit + 273.15
+    else:
+        total = (temp_unit + 273.15) * (9 / 5)
 
-print(f"{total:.2f}")
+    print(f"{total:.2f}")
+main()
--- 3025/3025.py
+++ 3025/3025.py
@@ -1,25 +1,27 @@
 '''season'''
+def main():
+    '''season'''
+    month = int(input())
+    day = int(input())
 
-month = int(input())
-day = int(input())
-s = 0
-if month in [1, 2, 3]:
-    s = "winter"
-elif month in [4, 5, 6]:
-    s = "spring"
-elif month in [7, 8, 9]:
-    s = "summer"
-elif month in [10, 11, 12]:
-    s = "fall"
-
-if not month % 3 and 21 <= day:
-    if s == "winter":
-        s = "spring"
-    elif s == "spring":
-        s = "summer"
-    elif s == "summer":
-        s = "fall"
-    elif s == "fall":
+    if month in [1, 2, 3]:
         s = "winter"
+    elif month in [4, 5, 6]:
+        s = "spring"
+    elif month in [7, 8, 9]:
+        s = "summer"
+    elif month in [10, 11, 12]:
+        s = "fall"
 
-print(s)
+    if not month % 3 and 21 <= day:
+        if s == "winter":
+            s = "spring"
+        elif s == "spring":
+            s = "summer"
+        elif s == "summer":
+            s = "fall"
+        elif s == "fall":
+            s = "winter"
+
+    print(s)
+main()
