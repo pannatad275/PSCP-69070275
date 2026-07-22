2ee89f57befcba7a7da9c94a22767fe7d2cbef61
pannatad275 <69070275@kmitl.ac.th>
2026-07-22 15:24:55 +0700

    s

---
+++ main.py
@@ -0,0 +1,10 @@
+role, age = input().split()
+age = float(age)
+
+if age < 18:
+    print("adawdw")
+elif age >= 18 and role == "true":
+    print("fdfse")
+elif age > 18 and role == "false":
+    print("fsef")
+
