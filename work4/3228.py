1ad4eac75611f8bbec54d66f88c6a85a5a1851e8
pannatad275 <69070275@kmitl.ac.th>
2026-08-14 23:03:57 +0700

    s

---
+++ work4/3228.py
@@ -0,0 +1,11 @@
+'''al'''
+def main():
+    '''al'''
+    name = input()
+    al = ['a','e','i','o','u']
+    count = 0
+    for i in name:
+        if i in al:
+            count +=1
+    print(count)
+main()
