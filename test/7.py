178b4a01791d3bdb67ba25de03037e8f9a427785
pannatad275 <69070275@kmitl.ac.th>
2026-08-17 01:48:45 +0700

    s

---
+++ test/7.py
@@ -0,0 +1,9 @@
+'''fac'''
+def main():
+    '''fac'''
+    n = int(input())
+    h = 1
+    for i in range(1,n+1):
+        h = h * i
+        print(h)
+main()
--- work4/3226.py
+++ work4/3226.py
@@ -1,12 +1,12 @@
 '''inflation'''
-import math as m
 def main():
-    '''infration'''
+    '''inflation'''
     n = float(input())
     k = int(input())
+    rate = 0.0381
     for _ in range(k):
-        inc = n * 0.0381
-        ans = m.floor(inc * 100 + 1e-9) /100
+        inc = n * rate
+        ans = int(inc * 100 + 1e-9) /100
         n += ans
     print(f'{n:.2f}')
 main()
